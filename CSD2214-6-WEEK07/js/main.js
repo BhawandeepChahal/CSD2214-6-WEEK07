8deace6881be54742e3bb6d2aea1b1e1126900a2
visualstudio <visualstudio@230-PC34>
2019-02-25 14:15:12 -0500

    final

--- CSD2214-6-WEEK07/js/main.js
+++ CSD2214-6-WEEK07/js/main.js
@@ -1,1 +1,12 @@
 ﻿// Your code here!
+// Your code here!
+function sum()
+{
+    var i = 117;
+    var sum = 0;
+    while (i >= 11) {
+        sum = sum + i;
+        return sum;
+        i = i - 8;
+    }
+}
